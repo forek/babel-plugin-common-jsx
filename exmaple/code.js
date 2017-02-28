37ce834377a0e665d4326d096290b7244768c21e
Forekid <only8623@hotmail.com>
2017-02-28 20:41:29 +0800

    Create code.js

---
+++ exmaple/code.js
@@ -0,0 +1,13 @@
+var Lab = '';
+
+var a = (
+  <div id="#main" name="box">
+    hello world!
+    <a href="/login">go</a>
+    <a href="/logout">close</a>
+    <Lab>My Lab</Lab>
+    <this>this class</this>
+    <this.main.classname to="/value" />
+    {true && 'tips'}
+  </div>
+);
